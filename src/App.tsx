8e1c3303bf53182b9237eb8405016797f4fb73b1
Filipe Pfluck <filipepfluckdev@gmail.com>
2023-03-06 11:58:17 -0300

    uploading code

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { ComponentStyleConfig } from "@chakra-ui/react"
+
+const styleObj: ComponentStyleConfig = {
+  baseStyle: {
+    bg: '',
+    display: ''
+  },
+  variants: {},
+}
+
+console.log(styleObj)
+
+function App() {
+  return (
+    <h1>Hello chakra</h1>
+  )
+}
+
+export default App
